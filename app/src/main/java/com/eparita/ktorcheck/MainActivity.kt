510235965bb6f3a49af732103b3e64d742057e52
paritadey <paritasampa95@gmail.com>
2023-07-09 23:31:23 +0530

    Ktor project setup initial commit

--- app/src/main/java/com/eparita/ktorcheck/MainActivity.kt
+++ app/src/main/java/com/eparita/ktorcheck/MainActivity.kt
@@ -2,10 +2,17 @@ package com.eparita.ktorcheck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.navigation.compose.rememberNavController
+import com.eparita.ktorcheck.ui.theme.BorutoAppTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            BorutoAppTheme {
+
+            }
+        }
     }
 }
