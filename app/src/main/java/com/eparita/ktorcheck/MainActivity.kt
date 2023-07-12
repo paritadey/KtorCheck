de68828f5a477295c61fc0c8b823942d1a103a6f
paritadey <paritasampa95@gmail.com>
2023-07-12 08:31:42 +0530

    Navgraph and Screens - setup

--- app/src/main/java/com/eparita/ktorcheck/MainActivity.kt
+++ app/src/main/java/com/eparita/ktorcheck/MainActivity.kt
@@ -3,15 +3,19 @@ package com.eparita.ktorcheck
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.eparita.ktorcheck.navigation.SetupNavGraph
 import com.eparita.ktorcheck.ui.theme.BorutoAppTheme
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavHostController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BorutoAppTheme {
-
+                navController= rememberNavController()
+                SetupNavGraph(navController = navController)
             }
         }
     }
---
+++ app/src/main/java/com/eparita/ktorcheck/util/Constants.kt
@@ -0,0 +1,5 @@
+package com.eparita.ktorcheck.util
+
+object Constants {
+    const val DETAILS_ARGUMENT_KEY = "heroId"
+}
